03925a9214d226b906797c0e716f1df20f2c54d5
manutonial <manumanusi2014@gmail.com>
2026-06-09 23:11:53 -0300

    feat(service): add method get_trade_by_symbol

--- app/services/trade_service.py
+++ app/services/trade_service.py
@@ -1,10 +1,14 @@
 
+from app.models.trade import Trade
 from app.repositories.trade_repository import TradeRepository
 
 
 class TradeService:
     def __init__(self, repository: TradeRepository) -> None:
         self.__repository = repository
+    
+    async def get_trades_by_symbol(self, symbol: str, limit: int) -> list[Trade]:
+        return await self.__repository.get_by_symbol(symbol, limit)
 
     async def bulk_insert_trades(self, trades: list[dict]) -> int:
         if not trades: 
