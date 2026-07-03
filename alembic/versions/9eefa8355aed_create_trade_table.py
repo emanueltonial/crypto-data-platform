d8728ffae46a46cec6d7347da426c80ae8250950
emanueltonial <emanueltonial77@outlook.com>
2026-07-03 02:50:22 -0300

    chore(lint): changes after ruff --fix

--- alembic/versions/9eefa8355aed_create_trade_table.py
+++ alembic/versions/9eefa8355aed_create_trade_table.py
@@ -5,17 +5,17 @@ Revises:
 Create Date: 2026-05-29 18:51:38.483636
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '9eefa8355aed'
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
